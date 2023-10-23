6d81b2b8aa7f15131ffafb21a9360bd52aecee15
Lawrence <lawrencehome21@gmail.com>
2023-10-23 14:07:39 -0700

    imported the form component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Header from "./components/Header";
-
+import Form from "./components/Form";
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Form></Form>
     </div>
   );
 }
