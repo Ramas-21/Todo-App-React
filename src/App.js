51d1d4022c4bc0362ca6e3b6113950a7898c1100
Lawrence <lawrencehome21@gmail.com>
2023-10-23 14:51:13 -0700

    imported the Todo component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Form from "./components/Form";
 import { useState } from "react";
+import Todo from "./components/Todo";
 function App() {
   const [todo, setTodo] = useState("");
   const [todoList, setTodoList] = useState([]);
