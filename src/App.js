a3d2c60042e17d60a219aa3f7ea00e3278745bbc
Lawrence <lawrencehome21@gmail.com>
2023-10-23 14:29:34 -0700

    passed props to the form component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import Header from "./components/Header";
 import Form from "./components/Form";
 import { useState } from "react";
 function App() {
-  const [todo,setTodo] = useState('');
-  const [todoList,setTodoList] = useState([]);
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
   return (
     <div className="App">
       <Header></Header>
-      <Form></Form>
+      <Form
+        todo={todo}
+        setTodo={setTodo}
+        todoList={todoList}
+        setTodoList={setTodoList}
+      ></Form>
     </div>
   );
 }
