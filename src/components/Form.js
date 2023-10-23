610d418373114d5bf49699733b157fdd8bd0a41a
Lawrence <lawrencehome21@gmail.com>
2023-10-23 14:36:02 -0700

    an event to handle onChange property

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,12 @@
 import styles from '../style.module.css';
 const Form = ({todo, setTodo, todoList, setTodoList}) => {
+    const handleChange = (event) =>{
+        setTodo(event.target.value);
+    }
     return(
         <div className={styles.todoForm}>
             <form>
-                <input value={todo} placeholder="Add todo item" className={styles.todoInput}></input>
+                <input value={todo} onChange={handleChange} placeholder="Add todo item" className={styles.todoInput}></input>
                 <button className={styles.todoButton}>Add</button>
             </form>
         </div>
