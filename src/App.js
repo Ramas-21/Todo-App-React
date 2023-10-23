5b1426745b7328f13a948eb3997f5751261f6b4f
Lawrence <lawrencehome21@gmail.com>
2023-10-23 14:24:25 -0700

    created todo state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Header from "./components/Header";
 import Form from "./components/Form";
+import { useState } from "react";
 function App() {
+  const [todo,setTodo] = useState('');
   return (
     <div className="App">
       <Header></Header>
