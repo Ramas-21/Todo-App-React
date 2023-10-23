96fcdae58b9b2706c91661a57cf2db9a3730a2a9
Lawrence <lawrencehome21@gmail.com>
2023-10-23 14:31:34 -0700

    destructured props

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,9 @@
 import styles from '../style.module.css';
-const Form = () => {
+const Form = ({todo, setTodo, todoList, setTodoList}) => {
     return(
         <div className={styles.todoForm}>
             <form>
-                <input placeholder="Add todo item" className={styles.todoInput}></input>
+                <input value={todo} placeholder="Add todo item" className={styles.todoInput}></input>
                 <button className={styles.todoButton}>Add</button>
             </form>
         </div>
